8ccc9087c8d63c6d6c561d9f04ccd0c5a136886e
Puytra <yanuarp21@gmail.com>
2020-01-29 10:19:18 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-image:url( '{{ asset('img/bg.jpg') }}' );
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #fff;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                color: #fff;
+                margin-bottom: 30px;
+            }
+
+            .table_responsive{
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Tugas Laravel
+                </div>
+
+                <div class="links">
+                    <a href=#>Oleh</a>
+                    <a href=#>Yanuar</a>
+                    <a href=#>Putra</a>
+                    <a href=#>Ardiansyah</a>
+                    <a href=#>Software</a>
+                    <a href=#>4</a>
+                    <a href=#>B</a><br><br>
+                    <a href=#>1 8 3 1 4 0 7 1 4 1 1 1 0 5 6</a>
+                    <!-- <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<!-- s -->
